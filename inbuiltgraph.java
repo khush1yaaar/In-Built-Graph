a01d6694c1484490beff165f325cd6e1d6e19a19
khushi soni <ksoni.param@gmail.com>
2024-06-04 14:54:29 +0530

    add & remove function done

--- inbuiltgraph.java
+++ inbuiltgraph.java
@@ -6,7 +6,16 @@ public class inbuiltgraph {
     static class Graph {
         ArrayList<ArrayList<Integer>> adj;
         int[][] matrix;
-        public Graph(ArrayList<Edge> edges, boolean isUndirected) {
+        int nodes;
+        public Graph() {
+
+        }
+        public Graph(ArrayList<Edge> edges, int nodes, boolean isUndirected) {
+            this.nodes = nodes;
+            adj = new ArrayList<>(nodes);
+            for(int i=0;i<nodes;i++) {
+                this.adj.add(new ArrayList<>());
+            }
             if(isUndirected) {
                 createUndirectedGraph(edges);
             }
@@ -15,32 +24,74 @@ public class inbuiltgraph {
             }
         }
         public void createDirectedGraph(ArrayList<Edge> edges) { // CREATE DIRECTED GRAPH
-
+            for(Edge e : edges) {
+                int u = e.u;
+                int v = e.v;
+                adj.get(u).add(v);
+            }
+            nodes = adj.size();
         }
         public void createUndirectedGraph(ArrayList<Edge> edges) { // CREATE UNDIRECTED GRAPH
+            for(Edge e : edges) {
+                int u = e.u;
+                int v = e.v;
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            nodes = adj.size();
+        }
+        public void add(int u, int v, boolean isUndirected) {
+            if (u >= nodes || v >= nodes) { 
+                for (int i = nodes; i <= Math.max(u, v); i++) {
+                    adj.add(new ArrayList<>());
+                }
+                nodes = Math.max(u, v) + 1; 
+            }
+            this.adj.get(u).add(v);
+            if(isUndirected) {
+                this.adj.get(v).add(u);
+            }
+            nodes = this.adj.size();
+        }
+        public void remove(int u, int v, boolean isUndirected) {
+            this.adj.get(u).remove(v);
+            if(isUndirected) {
+                this.adj.get(v).remove(u);
+            }
+            nodes = this.adj.size();
+        }
+        public int shortestPath(int src, int dest, boolean isNegativeWeighted) {
+            if(isNegativeWeighted) {
+                return bellManFord();
+            }
+            else {
+                return dijkstras();
+            }
+        }
 
-        }
-        public int shortestPath(int src, int dest) {
-            return -1;
-        }
         public boolean isCycle() { // CHECK IF THERE EXISTS A CYCLE
             return false;
         }
+
         public boolean DAG() { // CHECK IF IT'S A DIRECTED ACYCLIC GRAPH
             return false;
         }
+
         public ArrayList<Integer> dfs() { // RETURNS DFS TRAVERSAL
             ArrayList<Integer> ans = new ArrayList<>();
             return ans;
         }
+
         public ArrayList<Integer> bfs() { // RETURNS BFS TRAVERSAL
             ArrayList<Integer> ans = new ArrayList<>();
             return ans;
         }
+
         public ArrayList<Integer> topo() { // RETURN TOPO SORT
             ArrayList<Integer> ans = new ArrayList<>();
             return ans;
         }
+
         public boolean find(int x) { // CHECKS IF THE NODE EXISTS OR NOT
             return false;
         }
@@ -84,7 +135,19 @@ public class inbuiltgraph {
         edges.add(new Edge(4, 5));
         edges.add(new Edge(5, 6));
 
-        Graph graph = new Graph(edges, true);
-        graph.isCycle();
+        Graph graph = new Graph(edges, 7, true);
+        
+        System.out.println(graph.adj.get(0).get(0));
+        System.out.println(graph.nodes);
+        graph.add(7,6,true);
+        System.out.println(graph.adj.get(7).get(0));
+        System.out.println(graph.nodes);
     }
 }
+
+// Dijkstra’s Algorithm for shortest paths in weighted graphs.
+// Bellman-Ford Algorithm for graphs with negative weights.
+// Floyd-Warshall Algorithm for all-pairs shortest paths.
+// Tarjan’s Algorithm for finding strongly connected components.
+// Minimum Spanning Tree algorithms like Kruskal’s and Prim’s.
+
